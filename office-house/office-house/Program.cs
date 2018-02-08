88c66b81eb3471ae1d3f5d9456053bfa7f47d865
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-02-08 08:36:23 +0200

    created solution

---
+++ office-house/office-house/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace office_house
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
